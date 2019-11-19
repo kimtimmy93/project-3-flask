9d807be1668e3c1939ed2e34c854164e77dabe2b
kimtimmy93 <kimtimmy93@gmail.com>
2019-11-19 13:39:50 -0800

    add event post, put, update routes

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField()
-    is_admin = BooleanField
+    is_admin = BooleanField(null=False)
 
     class Meta:
         database = DATABASE
--- resources/events.py
+++ resources/events.py
@@ -18,6 +18,16 @@ def get_all_events():
     except models.DoesNotExist:
         return jsonify(data={}, status={"code": 401, "message":"Error getting the resource"})
 
+# POST
+@event.route('/', methods=["POST"])
+def create_events():
+    payload = request.get_json()
+
+    event = models.Event.create(**payload)
+
+    event_dict = model_to_dict(event)
+    return jsonify(data=event_dict, status={"code": 201, "message": "Success"})
+
 # Show
 @event.route('/<id>', methods=["GET"])
 def get_one_event(id):
@@ -26,5 +36,17 @@ def get_one_event(id):
 
     return jsonify(data=model_to_dict(event), status={"code": 200, "message": 'Success'})
 
+# Update
+@event.route('/<id>', methods=["PUT"])
+def update_event(id):
+    payload = request.get_json()
+
+    query = models.Event.update(**payload).where(models.Event.id == id)
+
+    event = models.Event.get_by_id(id)
+
+    event_dict = model_to_dict(event)
+
+    return jsonify(data=event_dict, status={"code": 200, "message": "resource updated successfully"})
 
 #Update, Edit, Delete for admin only
