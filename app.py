8b6c19a3baf7564cc5873cf4e374fccdbc034435
kimtimmy93 <kimtimmy93@gmail.com>
2019-11-20 12:59:40 -0800

    change origins to localhost 3000

--- app.py
+++ app.py
@@ -37,12 +37,12 @@ def after_request(response):
     g.db.close()
     return response
 
-CORS(event, origins=['http://localhost:3001'],
+CORS(event, origins=['http://localhost:3000'],
 supports_credentials=True)
 
 app.register_blueprint(event, url_prefix='/api/v1/events')
 
-CORS(user, origins=['http://localhost:3001'],
+CORS(user, origins=['http://localhost:3000'],
 supports_credentials=True)
 app.register_blueprint(user, url_prefix='/user')
 
