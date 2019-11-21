1b37441d859450a701afa9691542c77e6eabf079
Alex Raymond <alexraymond@Alexs-MacBook-Air.local>
2019-11-20 16:30:13 -0800

    routes work with no logout

--- resources/events.py
+++ resources/events.py
@@ -4,7 +4,7 @@ from playhouse.shortcuts import model_to_dict
 
 event = Blueprint('events', 'event')
 
-#Home Route
+#HOME ROUTE
 @event.route('/', methods=["GET"])
 def get_all_events():
     try:
@@ -14,7 +14,7 @@ def get_all_events():
     except models.DoesNotExist:
         return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
 
-#Post Route
+#POST ROUTE
 @event.route('/', methods=["POST"]) 
 def create_events():
     payload = request.get_json()
@@ -22,7 +22,7 @@ def create_events():
     event_dict = model_to_dict(event)
     return jsonify(data=event_dict, status={"code": 201, "message": "Success"})
 
-# SHOW ROUTE
+#SHOW ROUTE
 @event.route('/<id>', methods=["GET"])
 def get_one_event(id):
     print(id)
@@ -30,7 +30,7 @@ def get_one_event(id):
     return jsonify(data=model_to_dict(event), status={"code": 200, "message": "Success"})
 
 
-# Update
+#UPDATE ROUTE
 @event.route('/<id>', methods=["PUT"])
 def update_event(id):
     payload = request.get_json()
@@ -40,7 +40,7 @@ def update_event(id):
     event_dict = model_to_dict(event)
     return jsonify(data=event_dict, status={"code": 200, "message": "resource updated successfully"})
 
-# Delete
+#DELETE ROUTE
 @event.route('/<id>', methods=["DELETE"])
 def  delete_event(id):
     query = models.Event.delete().where(models.Event.id==id)
