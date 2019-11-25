2156180b32c2d3a864cc20b8ee9c5f4cf6d441ff
kimtimmy93 <kimtimmy93@gmail.com>
2019-11-25 15:35:21 -0800

    heroku

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from flask_login import UserMixin
 import os 
 from playhouse.db_url import connect
 
-# DATABASE = SqliteDatabase('events.sqlite')
-DATABASE = connect(os.environ.get('DATABASE_URL'))
+DATABASE = SqliteDatabase('events.sqlite')
+# DATABASE = connect(os.environ.get('DATABASE_URL'))
 
 class User(UserMixin, Model):
     username = CharField()
